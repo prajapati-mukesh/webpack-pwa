9b5bcf767055fb14da19ebdbb32dafaa1597f653
Mukesh Prajapati <mukesh.easternts@gmail.com>
2021-01-09 12:14:12 +0530

    Workbox offline

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import _ from 'lodash';
 import printMe from './print.js'
-
+if ('serviceWorker' in navigator) {
+   window.addEventListener('load', () => {
+     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+       console.log('SW registered: ', registration);
+     }).catch(registrationError => {
+       console.log('SW registration failed: ', registrationError);
+     });
+   });
+}
 function component() {
     const element = document.createElement('div');
     const btn = document.createElement('button');
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
- 
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
  module.exports = {
-   mode: 'development',
    entry: {
      index: './src/index.js',
      print: './src/print.js',
@@ -13,6 +13,12 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
       title: 'Webpack PWA',
     }),
     new CleanWebpackPlugin(),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
   ],
    output: {
      filename: '[name].bundle.js',
